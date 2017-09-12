de0548cecea8ef0c1a1ef753314e907221620ae7
Logan Klein <lkleindesigns@gmail.com>
2017-09-12 14:08:46 -0500

    rework environment variables and test cases

--- tests/index.controllers.test.js
+++ tests/index.controllers.test.js
@@ -3,84 +3,38 @@ const expect  = require('expect'),
 
 const {app} = require('./../app.js'),
       User  = require('./../models/user.model'),
-      Game  = require('./../controllers/games.controller'),
-      Index  = require('./../controllers/index.controller')
+      gameController  = require('./../controllers/games.controller'),
+      indexController  = require('./../controllers/index.controller')
+
+// ----- ROUTES ----- //
+var game_routes   = require('./../routes/games.routes')
+var review_routes = require('./../routes/reviews.routes')
+var index_routes  = require('./../routes/index.routes')
+
+app.use('/', index_routes)
+app.use('/games/:id/reviews', review_routes)
+app.use('/games', game_routes)
+// ----- ROUTES ----- //
 
 const {users, populateUsers} = require('./seeds/seeds.js')
 
 beforeEach(populateUsers)
-console.log(users)
 
-describe('POST /register - createUser', () => {
-  it('should create new user', (done) => {
-    var email = 'example@example.com'
+describe('POST /register', () => {
+  it('should create a new user', (done) => {
+    var username = 'logan@example.com'
     var password = 'abc123'
 
     request(app)
       .post('/register')
-      .send({email, password})
+      .send({username, password})
       .expect(200)
       .expect((res) => {
         expect(res.body._id).toExist()
-        expect(res.body.email).toBe(email)
+        expect(res.body.username).toBe(username)
       })
-      .end((err) => {
-        if (err) {
-          return done(err)
-        }
-
-        User.findOne({email}).then((user) => {
-          expect(user).toExist()
-          expect(user.password).toNotBe(password)
-          done()
-        }).catch((e) => done(e))
-      })
-  })
-
-  it('should not save duplicate user', (done) => {
-    request(app)
-      .post('/register')
-      .send({
-        email: users[0].email,
-        password: 'Password123'
-      })
-      .expect(400)
-      .end(done)
-  })
-
-  it('should return validation error if request invalid', (done) => {
-    request(app)
-      .post('/register')
-      .send({
-        email: 1,
-        password: undefined
-      })
-      .expect(400)
       .end(done)
   })
 })
 
 
-describe('POST /login - loginUser', () => {
-  it('should login user', (done) => {
-    request(app)
-      .post('/login')
-      .send({
-        email: users[1].email,
-        password: users[1].password
-      })
-      .expect(200)
-      .end(done)
-  })
-
-  it('should reject invalid login', (done) => {
-    request(app)
-      .post('/login')
-      .send({
-        email: users[1].email,
-        password: 'alk;fjalkfj'
-      })
-      .expect(400)
-      .end(done)
-  })
-})
